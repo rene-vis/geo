ab01922e56be1dc3d9f399f4956ea4b521036eea
terry <terrychaoc@gmail.com>
2023-06-01 19:38:22 +0800

    feat: add transformation2d

---
+++ transformation2d/transformation2d.cpp
@@ -0,0 +1,8 @@
+#include "../geom.h"
+#include "transformation2d.h"
+
+_GEOM_BEGIN
+
+Transformation2d::Transformation2d(){}
+
+_GEOM_END
---
+++ transformation2d/transformation2d.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "../geom.h"
+
+_GEOM_BEGIN
+
+class Transformation2d{
+public:
+    Transformation2d();
+private:
+};
+
+_GEOM_END
